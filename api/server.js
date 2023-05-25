40ce24e3cb5e54a3cbb99eb0741e8e880e86b17b
yamakalyan <yamakalyan6@gmail.com>
2023-05-25 13:33:50 -0700

    uploaded

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const admin = require('./controllers/Admin')
 env.config()
 
 app.use(cors({
-    origin : 'https://yamakalyan.github.io/blood_donation_app/'
+    origin : '*'
 }))
 
 app.listen(process.env.PORT, (req, res)=>{
